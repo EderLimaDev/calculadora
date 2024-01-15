47ac63ef727e163ceaa915c6922798540a71aca3
EderLimaDev <ederlimarh@gmail.com>
2024-01-15 09:44:31 -0300

    criando lógica do cálculo e inclusao no display

--- app.js
+++ app.js
@@ -1,32 +1,140 @@
 
+let x = 0;
+let y = 0;
+let newOperator = '';
+let result = '';
 
 // Função para apagar
 
 function backspace() {
-    var tela = document.querySelector(".tela").innerHTML;
-    document.querySelector(".tela").innerHTML = tela.substring(0, tela.length - 1);
+    const tela = document.querySelector(".visor-parcial").innerHTML;
+    const telaCompleta = document.querySelector(".visor-calculo-completo").innerHTML; 
     
+    if(result === '' && tela !== NaN) {
+        if(tela === NaN) {
+            newOperator = ''
+            document.querySelector(".visor-calculo-completo").innerHTML = telaCompleta.substring(0, telaCompleta.length - 1);
+        } else {
+            document.querySelector(".visor-parcial").innerHTML = tela.substring(0, tela.length - 1);
+            document.querySelector(".visor-calculo-completo").innerHTML = telaCompleta.substring(0, telaCompleta.length - 1);
+        }
+    } else {
+        if(tela !== '') {
+            document.querySelector(".visor-parcial").innerHTML = tela.substring(0, tela.length - 1);
+        } else {
+            result = '';
+        }
+    }   
+   
 }
 
 // Função para incluir os dados na tela
 
 function btnPressed(num) {
-   var number = document.querySelector(".tela").innerHTML;
-   document.querySelector(".tela").innerHTML = number + num;
-    
+   const number = document.querySelector(".visor-parcial").innerHTML;
+   document.querySelector(".visor-parcial").innerHTML = number + num;
+
+   const number2 = document.querySelector(".visor-calculo-completo").innerHTML;
+   document.querySelector(".visor-calculo-completo").innerHTML = number2 + num;
+
 }
+
+// Função para selecionar operador
+
+function operator(selectedOperator) {
+    switch (selectedOperator) {
+        case "+":
+            if(document.querySelector(".visor-parcial").innerHTML === '') {
+                x = Number(document.querySelector(".visor-calculo-completo").innerHTML)
+                newOperator = sum
+                document.querySelector(".visor-calculo-completo").innerHTML = x + selectedOperator
+            } else {
+                x = Number(document.querySelector(".visor-parcial").innerHTML)
+                newOperator = sum
+                document.querySelector(".visor-parcial").innerHTML = '';
+                document.querySelector(".visor-calculo-completo").innerHTML = x + selectedOperator
+            }
+            break;
+            
+        case "-": 
+
+             if(document.querySelector(".visor-parcial").innerHTML === '') {
+                x = Number(document.querySelector(".visor-calculo-completo").innerHTML)
+                newOperator = subtraction
+                document.querySelector(".visor-calculo-completo").innerHTML = x + selectedOperator
+            } else {
+                x = Number(document.querySelector(".visor-parcial").innerHTML)
+                newOperator = subtraction
+                document.querySelector(".visor-parcial").innerHTML = '';
+                document.querySelector(".visor-calculo-completo").innerHTML = x + selectedOperator
+            }
+            break;
+
+        case "÷":
+            if(document.querySelector(".visor-parcial").innerHTML === '') {
+                x = Number(document.querySelector(".visor-calculo-completo").innerHTML)
+                newOperator = division
+                document.querySelector(".visor-calculo-completo").innerHTML = x + selectedOperator
+            } else { 
+            x = Number(document.querySelector(".visor-parcial").innerHTML)
+            newOperator = division
+            document.querySelector(".visor-parcial").innerHTML = '';
+            document.querySelector(".visor-calculo-completo").innerHTML = x + selectedOperator
+            }
+            break;
+
+        case "x":
+            if(document.querySelector(".visor-parcial").innerHTML === '') {
+                x = Number(document.querySelector(".visor-calculo-completo").innerHTML)
+                newOperator = multiply
+                document.querySelector(".visor-calculo-completo").innerHTML = x + selectedOperator
+            } else { 
+            x = Number(document.querySelector(".visor-parcial").innerHTML)
+            newOperator = multiply
+            document.querySelector(".visor-parcial").innerHTML = '';
+            document.querySelector(".visor-calculo-completo").innerHTML = x + selectedOperator
+            }
+            break;
+
+        default:
+            break;
+    }
+} 
 
 // Função para calcular
 
-function calculo() {
-    var resultado = document.querySelector(".tela").innerHTML;
-
-    
+function calculate(x, operador, y) {
+   y = Number(document.querySelector(".visor-parcial").innerHTML)
+   document.querySelector(".visor-parcial").innerHTML = '';
+   result = document.querySelector(".visor-parcial").innerHTML = operador(x, y)
+   console.log(result);
 }
+
+function sum(x, y) {
+    return x + y
+}
+
+function subtraction(x, y) {
+    return x - y
+}
+
+function multiply(x, y) {
+    return x * y
+}
+
+function division(x, y) {
+    return x / y
+}
+
+
+
 
 // Função para limpar
 
 function clearTela() {
     document.querySelector('.tela').innerHTML = "";
+    x = 0;
+    y = 0;
+    newOperator = '';
 
 }
